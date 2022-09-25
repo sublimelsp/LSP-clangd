eba6f587a4000cb74ac3d5ceeb9196fa9437c280
Lucas Alber <lucasd.alber@gmail.com>
2022-09-25 23:39:07 +0200

    Implement server configuration

--- plugin.py
+++ plugin.py
@@ -1,13 +1,41 @@
 from LSP.plugin import AbstractPlugin, register_plugin, unregister_plugin, WorkspaceFolder, ClientConfig
-from LSP.plugin.core.typing import Optional, Dict, List
+from LSP.plugin.core.typing import Any, Optional, Dict, List
 
 import os
 import shutil
 
 import sublime
 
+
 SESSION_NAME = "clangd"
 STORAGE_DIR = "LSP-clangd"
+GITHUB_DL_URL = 'https://github.com/clangd/clangd/releases/download/'\
+                + '{release_tag}/clangd-{platform}-{release_tag}.zip'
+GITHUB_RELEASE = '15.0.1'
+
+CLANGD_SETTING_PREFIX = "clangd."
+CLANGD_SETTING_TO_ARGUMENT = {
+    "number-workers": "-j"
+}
+
+
+def get_argument_for_setting(settings_key: str) -> str:
+    """
+    Returns the command argument for a `clangd.*` key.
+    """
+    key = settings_key[len(CLANGD_SETTING_PREFIX):]
+    return CLANGD_SETTING_TO_ARGUMENT.get(key, "--" + key)
+
+
+def get_settings() -> sublime.Settings:
+    return sublime.load_settings("LSP-clangd.sublime-settings")
+
+
+def get_clangd_settings() -> Dict[str, Any]:
+    # Workaround: The 3.3 host does not provide an API to iterate over all keys.
+    defaults = sublime.decode_value(sublime.load_resource("Packages/LSP-clangd/LSP-clangd.sublime-settings"))
+    settings = get_settings()
+    return {k: settings.get(k) for k in defaults.keys() if k.startswith(CLANGD_SETTING_PREFIX)}
 
 
 class Clangd(AbstractPlugin):
@@ -47,7 +75,21 @@ class Clangd(AbstractPlugin):
         workspace_folders: List[WorkspaceFolder],
         configuration: ClientConfig,
     ) -> Optional[str]:
-        # TODO: Append arguments to command depending on settings
+
+        # The configuration is persisted
+        # reset the command to prevent adding a arguments multiple times
+        configuration.command = [cls.clangd_path()]
+
+        for settings_key, value in get_clangd_settings().items():
+            if not value:
+                # False or None
+                continue
+            elif value is True:
+                configuration.command.append(get_argument_for_setting(settings_key))
+            elif isinstance(value, str):
+                configuration.command.append("{key}={value}".format(key=get_argument_for_setting(settings_key), value=value))
+            else:
+                raise TypeError("Type {} not supported for setting {}.".format(str(type(value)), settings_key))
         return None
 
 
